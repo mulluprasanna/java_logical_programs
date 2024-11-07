8cb4d2246d67c45d5b2429dffe05f0f7a6001e76
mulluprasanna <145213376+mulluprasanna@users.noreply.github.com>
2024-11-07 13:22:32 +0530

    Add files via upload

---
+++ Day-1/Emp.java
@@ -0,0 +1,12 @@
+class Emp
+{
+public static void main(String args[])
+{
+int Empno=101;
+String EmpName="Prasanna";
+Float EmpSal=100.64F;
+System.out.println("The Given Employee Number is :"+Empno);
+System.out.println("The Given Employee Name is :"+EmpName);
+System.out.println("The Given Employee Salary is :"+EmpSal);
+}
+}
